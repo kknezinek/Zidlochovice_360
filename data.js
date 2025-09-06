91399ebc4d68f5c31f074e083e275895695c78c1
kknezinek <84154627+kknezinek@users.noreply.github.com>
2025-09-06 21:05:25 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-dji_0862",
+      "name": "DJI_0862",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.052951504770014424,
+          "pitch": 0.3265886506564275,
+          "rotation": 4.71238898038469,
+          "target": "1-dji_0868"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-dji_0868",
+      "name": "DJI_0868",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5334432489691121,
+          "pitch": 0.21882423580379395,
+          "rotation": 7.0685834705770345,
+          "target": "0-dji_0862"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
